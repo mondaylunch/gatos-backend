c70c5588aea8b910a3b30be804d956847faa7cbd
Paul Makles <me@insrt.uk>
2023-01-15 18:46:35 +0000

    feat: initialise spring boot project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Gatos Backend
+
+## Running API Server
+
+Start the API:
+
+```bash
+./gradlew :api:bootRun
+```
+
+Navigate to http://localhost:8080
+
+## Run Tests
+
+```bash
+./gradlew :api:test
+```
---
+++ api/src/test/java/gay/oss/gatos/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gay.oss.gatos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
