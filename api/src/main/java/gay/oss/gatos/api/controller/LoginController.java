1d056f6c9c700304ab93bc5a3e266924c4aefff9
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-01-23 20:42:17 +0000

    feat: demo API with controller and repository
    - navigate to /login/username/<insert_username>

--- api/src/main/java/gay/oss/gatos/api/controller/LoginController.java
+++ api/src/main/java/gay/oss/gatos/api/controller/LoginController.java
@@ -1,5 +1,31 @@
 package gay.oss.gatos.api.controller;
 
+// imports from other packages
+import gay.oss.gatos.api.model.UserModel;
+import gay.oss.gatos.api.repository.LoginRepository;
+import gay.oss.gatos.api.exceptions.UserNotFoundException;
+
+// rest api stuff do not touch 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
-    
+
+    private final LoginRepository repository;
+
+    @Autowired
+    public LoginController(LoginRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/username/{username}")
+    public String findByUsername(@PathVariable String username) throws UserNotFoundException {
+        return this.repository.findByUsername(username);
+    }
+
 }
--- api/src/main/java/gay/oss/gatos/api/repository/LoginRepository.java
+++ api/src/main/java/gay/oss/gatos/api/repository/LoginRepository.java
@@ -1,5 +1,18 @@
 package gay.oss.gatos.api.repository;
 
+import org.springframework.stereotype.Repository;
+
+import gay.oss.gatos.api.model.UserModel;
+
+@Repository
 public class LoginRepository {
-    
+    //TODO: make it work after insert is done with the db.
+
+    // just a demo
+    public String findByUsername(String username) {
+        if(username.equals("ff")){
+            return "you found me!!!";
+        }
+        return "you didn't find me :(";
+    }
 }
