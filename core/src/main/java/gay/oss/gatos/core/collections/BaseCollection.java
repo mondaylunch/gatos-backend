852dae90018c11090b547b3d5a96348200f59a4c
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-01-24 20:05:33 +0000

    feat: add a method to get database objects based on a field value

--- core/src/main/java/gay/oss/gatos/core/collections/BaseCollection.java
+++ core/src/main/java/gay/oss/gatos/core/collections/BaseCollection.java
@@ -3,41 +3,44 @@ package gay.oss.gatos.core.collections;
 import com.mongodb.client.MongoCollection;
 import gay.oss.gatos.core.Database;
 import gay.oss.gatos.core.models.BaseModel;
+import org.bson.conversions.Bson;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.mongodb.client.model.Filters.eq;
 
 /**
- * Common Database collection queries
+ * Common Database collection queries.
  */
 public class BaseCollection<T extends BaseModel> {
 
     private final MongoCollection<T> collection;
 
     /**
-     * Construct a new BaseCollection
+     * Construct a new BaseCollection.
      *
-     * @param collectionName Collection name
-     * @param cls            Model class
+     * @param collectionName Collection name.
+     * @param cls            Model class.
      */
     public BaseCollection(String collectionName, Class<T> cls) {
         collection = Database.getCollection(collectionName, cls);
     }
 
     /**
-     * Get the underlying Mongo Collection
+     * Get the underlying Mongo Collection.
      *
-     * @return underlying Mongo Collection
+     * @return underlying Mongo Collection.
      */
     public MongoCollection<T> getCollection() {
         return collection;
     }
 
     /**
-     * Insert a new document into the collection
+     * Insert a new document into the collection.
      *
-     * @param obj POJO
+     * @param obj The POJO.
      */
     public void insert(T obj) {
         if (obj.getId() == null) {
@@ -47,12 +50,37 @@ public class BaseCollection<T extends BaseModel> {
     }
 
     /**
-     * Get a document by its UUID
+     * Gets a document.
      *
-     * @param id UUID
-     * @return POJO
+     * @param id The ID of the document to get.
+     * @return The POJO.
      */
-    public T getById(UUID id) {
-        return getCollection().find(eq("_id", id)).first();
+    public T get(UUID id) {
+        return getCollection().find(idFilter(id)).first();
+    }
+
+    /**
+     * Get documents by a field.
+     *
+     * @param field The field name.
+     * @param value The field value.
+     * @return a {@code List} of POJOs.
+     */
+    public List<T> get(String field, Object value) {
+        List<T> list = new ArrayList<>();
+        for (T obj : getCollection().find(eq(field, value))) {
+            list.add(obj);
+        }
+        return list;
+    }
+
+    /**
+     * Creates an ID filter.
+     *
+     * @param id The ID to filter by.
+     * @return The filter.
+     */
+    private static Bson idFilter(UUID id) {
+        return eq("_id", id);
     }
 }
