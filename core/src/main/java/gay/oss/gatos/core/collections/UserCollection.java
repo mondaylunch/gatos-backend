f401f47cc7fadf2f7689a687ed2bf1be161a5056
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-01-24 23:03:52 +0000

    chore: create a username filter and a method getUser

--- core/src/main/java/gay/oss/gatos/core/collections/UserCollection.java
+++ core/src/main/java/gay/oss/gatos/core/collections/UserCollection.java
@@ -2,6 +2,9 @@ package gay.oss.gatos.core.collections;
 
 import gay.oss.gatos.core.models.User;
 
+import org.bson.conversions.Bson;
+import static com.mongodb.client.model.Filters.eq;
+
 /**
  * "users" collection for {@link User}
  */
@@ -10,4 +13,25 @@ public class UserCollection extends BaseCollection<User> {
     public UserCollection() {
         super("users", User.class);
     }
+    
+    /**
+     * Creates a username filter.
+     *
+     * @param String The username to filter by.
+     * @return The filter.
+     */
+    private static Bson usernameFilter(String username) {
+        return eq("username", username);
+    }
+
+    /**
+     * Gets a document.
+     *
+     * @param username The username of the user.
+     * @return The POJO.
+     */
+    public User getUser(String username) {
+        return getCollection().find(usernameFilter(username)).first();
+    }
+
 }
