18aae0eef17cbcb48760f5ed6522bc2c6be067cd
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-01-24 20:09:09 +0000

    feat: add database update and delete methods

--- core/src/main/java/gay/oss/gatos/core/collections/BaseCollection.java
+++ core/src/main/java/gay/oss/gatos/core/collections/BaseCollection.java
@@ -1,13 +1,20 @@
 package gay.oss.gatos.core.collections;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Updates;
 import gay.oss.gatos.core.Database;
 import gay.oss.gatos.core.models.BaseModel;
 import org.bson.conversions.Bson;
+import org.jetbrains.annotations.Nullable;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -75,6 +82,30 @@ public class BaseCollection<T extends BaseModel> {
     }
 
     /**
+     * Updates a document. Only non-null fields will be updated.
+     *
+     * @param id  The ID of the document to update.
+     * @param obj The POJO to update with.
+     * @return The updated POJO.
+     */
+    public T update(UUID id, T obj) {
+        List<Bson> updates = getNonNullUpdates(obj);
+        if (!updates.isEmpty()) {
+            getCollection().updateOne(idFilter(id), Updates.combine(updates));
+        }
+        return get(id);
+    }
+
+    /**
+     * Deletes a document.
+     *
+     * @param id The ID of the document to delete.
+     */
+    public void delete(UUID id) {
+        getCollection().deleteOne(idFilter(id));
+    }
+
+    /**
      * Creates an ID filter.
      *
      * @param id The ID to filter by.
@@ -82,5 +113,82 @@ public class BaseCollection<T extends BaseModel> {
      */
     private static Bson idFilter(UUID id) {
         return eq("_id", id);
+    }
+
+    /**
+     * Creates a list of non-null updates from a POJO.
+     *
+     * @param obj The POJO.
+     * @return A list of updates.
+     */
+    private static List<Bson> getNonNullUpdates(Object obj) {
+        return createPropertyDescriptorStream(obj.getClass())
+            .filter(BaseCollection::hasGetter)
+            .map(descriptor -> getField(descriptor, obj))
+            .filter(Objects::nonNull)
+            .map(Field::toUpdate)
+            .toList();
+    }
+
+    /**
+     * Creates a stream of {@code PropertyDescriptors} from a class. The {@link Object} class is excluded.
+     *
+     * @param clazz The class.
+     * @return A stream of {@code PropertyDescriptors}.
+     */
+    private static Stream<PropertyDescriptor> createPropertyDescriptorStream(Class<?> clazz) {
+        try {
+            return Stream.of(Introspector.getBeanInfo(clazz, Object.class).getPropertyDescriptors());
+        } catch (IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Checks if a {@code PropertyDescriptor} has a getter.
+     *
+     * @param descriptor The {@code PropertyDescriptor}.
+     * @return {@code true} if the {@code PropertyDescriptor} has a getter, {@code false} otherwise.
+     */
+    private static boolean hasGetter(PropertyDescriptor descriptor) {
+        return descriptor.getReadMethod() != null;
+    }
+
+    /**
+     * Creates a {@code Field} object by invoking the getter of a {@code PropertyDescriptor} on the passed object.
+     *
+     * @param descriptor The {@code PropertyDescriptor}.
+     * @param obj        The object to invoke the getter on.
+     * @return A {@code Field} object.
+     */
+    @Nullable
+    private static Field getField(PropertyDescriptor descriptor, Object obj) {
+        try {
+            Object value = descriptor.getReadMethod().invoke(obj);
+            if (value != null) {
+                return new Field(descriptor.getName(), value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Represents a field of an object.
+     *
+     * @param name  The name of the field.
+     * @param value The value of the field.
+     */
+    private record Field(String name, Object value) {
+
+        /**
+         * Creates a MongoDB update.
+         *
+         * @return The update.
+         */
+        private Bson toUpdate() {
+            return Updates.set(name, value);
+        }
     }
 }
