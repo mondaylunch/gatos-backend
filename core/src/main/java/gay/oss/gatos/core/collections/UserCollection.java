8665cfdb56a41876590408f69ef6de4144237318
Paul Makles <me@insrt.uk>
2023-01-24 22:10:48 +0000

    feat: basic header authentication to dummy user

--- core/src/main/java/gay/oss/gatos/core/collections/UserCollection.java
+++ core/src/main/java/gay/oss/gatos/core/collections/UserCollection.java
@@ -1,13 +1,21 @@
 package gay.oss.gatos.core.collections;
 
+import java.util.UUID;
+
 import gay.oss.gatos.core.models.User;
 
 /**
  * "users" collection for {@link User}
  */
 public class UserCollection extends BaseCollection<User> {
-
     public UserCollection() {
         super("users", User.class);
+    }
+
+    public User authenticate(String token) throws RuntimeException {
+        User user = new User();
+        user.setId(new UUID(0, 0));
+
+        return user;
     }
 }
