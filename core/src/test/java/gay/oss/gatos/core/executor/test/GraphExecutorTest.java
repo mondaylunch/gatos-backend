a8ba7dc8f93b249f9c2f0979ba69927d6fd4a4f2
williambl <william@williambl.com>
2023-01-25 18:08:59 +0000

    test: add more complex graphexecutor test

--- core/src/test/java/gay/oss/gatos/core/executor/test/GraphExecutorTest.java
+++ core/src/test/java/gay/oss/gatos/core/executor/test/GraphExecutorTest.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Assertions;
@@ -24,6 +25,7 @@ import gay.oss.gatos.core.graph.data.DataType;
 public class GraphExecutorTest {
     private static final NodeType ADD_INTS = new AddIntNodeType();
     private static final NodeType ADD_INTS_SLOWLY = new AddIntNodeType();
+    private static final NodeType ADD_INTS_2INPUT = new AddIntTwoInputNodeType();
     private static final NodeType INPUT_INT = new InputIntNodeType();
 
     @Test
@@ -96,6 +98,56 @@ public class GraphExecutorTest {
         Assertions.assertEquals(15, result.get());
     }
 
+    @Test
+    public void complexGraphComputesCorrectly() {
+        var graph = new Graph();
+        AtomicInteger result = new AtomicInteger();
+        var output = graph.addNode(new OutputIntNodeType(result));
+
+        var inputs = IntStream.of(2, 4, 8, 16, 2)
+            .mapToObj(i -> {
+                var input = graph.addNode(INPUT_INT);
+                return graph.modifyNode(input.id(), node -> node.modifySetting("value", i));
+            })
+            .toList();
+
+        var adder1 = graph.addNode(ADD_INTS_2INPUT);
+        var adder2 = graph.addNode(ADD_INTS_2INPUT);
+        var adder3 = graph.addNode(ADD_INTS_2INPUT);
+        var adder4 = graph.addNode(ADD_INTS_2INPUT);
+
+        connectInt(graph, inputs.get(1), "out", adder1, "in1");
+        connectInt(graph, inputs.get(2), "out", adder1, "in2");
+
+        connectInt(graph, inputs.get(3), "out", adder2, "in1");
+        connectInt(graph, inputs.get(4), "out", adder2, "in2");
+
+        connectInt(graph, adder1, "out", adder3, "in1");
+        connectInt(graph, adder2, "out", adder3, "in2");
+
+        connectInt(graph, adder3, "out", adder4, "in1");
+        connectInt(graph, inputs.get(0), "out", adder4, "in2");
+
+        connectInt(graph, adder4, "out", output, "in");
+
+        var executionOrderedNodes = graph.getExecutionOrder();
+        Assertions.assertTrue(executionOrderedNodes.isPresent());
+        var graphExecutor = new GraphExecutor(executionOrderedNodes.get(), graph.getConnections());
+
+        CompletableFuture.runAsync(graphExecutor.execute()).join();
+        Assertions.assertEquals(32, result.get());
+    }
+
+    private static void connectInt(Graph graph, Node a, String connectorA, Node b, String connectorB) {
+        var conn = NodeConnection.createConnection(
+            a, connectorA,
+            b, connectorB,
+            DataType.INTEGER
+        );
+        Assertions.assertTrue(conn.isPresent());
+        graph.addConnection(conn.get());
+    }
+
     private static final class AddIntNodeType implements NodeType {
         @Override
         public NodeCategory category() {
@@ -130,6 +182,43 @@ public class GraphExecutorTest {
                         DataBox.get(settings, "value_to_add", DataType.INTEGER).orElseThrow()
                         + DataBox.get(inputs, "in", DataType.INTEGER).orElseThrow()
                     ))
+            );
+        }
+    }
+
+    private static final class AddIntTwoInputNodeType implements NodeType {
+        @Override
+        public NodeCategory category() {
+            return NodeCategory.PROCESS;
+        }
+
+        @Override
+        public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> state) {
+            return Set.of(
+                new NodeConnector.Input<>(nodeId, "in1", DataType.INTEGER),
+                new NodeConnector.Input<>(nodeId, "in2", DataType.INTEGER)
+            );
+        }
+
+        @Override
+        public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> state) {
+            return Set.of(
+                new NodeConnector.Output<>(nodeId, "out", DataType.INTEGER)
+            );
+        }
+
+        @Override
+        public Map<String, DataBox<?>> settings() {
+            return Map.of();
+        }
+
+        @Override
+        public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings) {
+            return Map.of(
+                "out", CompletableFuture.completedFuture(DataType.INTEGER.create(
+                    DataBox.get(inputs, "in1", DataType.INTEGER).orElseThrow()
+                        + DataBox.get(inputs, "in2", DataType.INTEGER).orElseThrow()
+                ))
             );
         }
     }
