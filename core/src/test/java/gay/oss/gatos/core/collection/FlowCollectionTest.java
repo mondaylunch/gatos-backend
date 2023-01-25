9887a67ffe93daa7ee8271d8e97c5d42afc7a750
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-01-25 16:09:54 +0000

    test: use relative count changes instead of fixed count

--- core/src/test/java/gay/oss/gatos/core/collection/FlowCollectionTest.java
+++ core/src/test/java/gay/oss/gatos/core/collection/FlowCollectionTest.java
@@ -16,7 +16,6 @@ public class FlowCollectionTest {
     @BeforeEach
     void setUp() {
         reset();
-        assertFlowCount(0);
     }
 
     @AfterEach
@@ -33,8 +32,9 @@ public class FlowCollectionTest {
         Flow flow = createFlow();
         String flowName = flow.getName();
         UUID authorId = flow.getAuthorId();
+        long countBefore = getFlowCount();
         objects.insert(flow);
-        assertFlowCount(1);
+        assertFlowCount(countBefore + 1);
         Flow retrievedFlow = objects.get(flow.getId());
         Assertions.assertEquals(flowName, retrievedFlow.getName());
         Assertions.assertEquals(authorId, retrievedFlow.getAuthorId());
@@ -47,8 +47,9 @@ public class FlowCollectionTest {
         UUID authorId = flow.getAuthorId();
         UUID id = UUID.randomUUID();
         flow.setId(id);
+        long countBefore = getFlowCount();
         objects.insert(flow);
-        assertFlowCount(1);
+        assertFlowCount(countBefore + 1);
         Flow retrievedFlow = objects.get(flow.getId());
         Assertions.assertEquals(flowName, retrievedFlow.getName());
         Assertions.assertEquals(authorId, retrievedFlow.getAuthorId());
@@ -65,29 +66,32 @@ public class FlowCollectionTest {
         String flowName = "Test Flow";
         UUID authorId = UUID.randomUUID();
         Flow flow = new Flow(flowName, authorId);
+        long countBefore = getFlowCount();
         objects.insert(flow);
+        assertFlowCount(countBefore + 1);
         Assertions.assertThrows(MongoWriteException.class, () -> objects.insert(flow));
-        assertFlowCount(1);
     }
 
     @Test
     public void canHaveDuplicateName() {
         String flowName = "Test Flow";
         Flow flow = new Flow(flowName, UUID.randomUUID());
+        long countBefore = getFlowCount();
         objects.insert(flow);
         Flow flow2 = new Flow(flowName, UUID.randomUUID());
         Assertions.assertDoesNotThrow(() -> objects.insert(flow2));
-        assertFlowCount(2);
+        assertFlowCount(countBefore + 2);
     }
 
     @Test
     public void canHaveDuplicateAuthorId() {
         UUID authorId = UUID.randomUUID();
         Flow flow = new Flow("Test Flow", authorId);
+        long countBefore = getFlowCount();
         objects.insert(flow);
         Flow flow2 = new Flow("Test Flow 2", authorId);
         Assertions.assertDoesNotThrow(() -> objects.insert(flow2));
-        assertFlowCount(2);
+        assertFlowCount(countBefore + 2);
     }
 
     @Test
@@ -137,20 +141,26 @@ public class FlowCollectionTest {
     @Test
     public void canDeleteFlow() {
         Flow flow = createFlow();
+        long countBefore = getFlowCount();
         objects.insert(flow);
-        assertFlowCount(1);
+        assertFlowCount(countBefore + 1);
         objects.delete(flow.getId());
-        assertFlowCount(0);
+        assertFlowCount(countBefore);
     }
 
     @Test
     public void canDeleteNonExistentFlow() {
+        long countBefore = getFlowCount();
         objects.delete(UUID.randomUUID());
-        assertFlowCount(0);
+        assertFlowCount(countBefore);
     }
 
-    private void assertFlowCount(int count) {
-        Assertions.assertEquals(count, objects.getCollection().countDocuments());
+    private long getFlowCount() {
+        return objects.getCollection().countDocuments();
+    }
+
+    private void assertFlowCount(long count) {
+        Assertions.assertEquals(count, getFlowCount());
     }
 
     private Flow createFlow() {
