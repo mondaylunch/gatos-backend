f84596f97010bb8ebcc6a0d1183162bdf50e9592
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-01-26 19:08:37 +0000

    chore: make erros return a JSON object

--- api/src/main/java/gay/oss/gatos/api/exceptions/UsernameAlreadyInUseException.java
+++ api/src/main/java/gay/oss/gatos/api/exceptions/UsernameAlreadyInUseException.java
@@ -1,9 +1,17 @@
 package gay.oss.gatos.api.exceptions;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UsernameAlreadyInUseException extends Exception {
+
+    public static HashMap<String, String> getErrorAsJSON() {
+        HashMap<String, String> error = new HashMap<>();
+        error.put("error", "Username Already In Use");
+        return error;
+    }
 
 }
