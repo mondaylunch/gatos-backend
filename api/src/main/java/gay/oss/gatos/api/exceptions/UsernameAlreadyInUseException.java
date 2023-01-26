a3480fe132bf93b8fe46110df0faa7aa13bed672
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-01-26 13:17:59 +0000

    chore: change response status to BAD_REQUEST

--- api/src/main/java/gay/oss/gatos/api/exceptions/UsernameAlreadyInUseException.java
+++ api/src/main/java/gay/oss/gatos/api/exceptions/UsernameAlreadyInUseException.java
@@ -3,7 +3,7 @@ package gay.oss.gatos.api.exceptions;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UsernameAlreadyInUseException extends Exception {
 
 }
