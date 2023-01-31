5dd3c4e0b4e8510f8bf1b213d64e36beacc60053
fenn7 <afenn666@gmail.com>
2023-01-31 12:22:49 +0000

    chore: remove unused code

--- api/src/main/java/gay/oss/gatos/api/repository/LoginRepository.java
+++ api/src/main/java/gay/oss/gatos/api/repository/LoginRepository.java
@@ -20,7 +20,6 @@ public class LoginRepository {
             throw new UserNotFoundException();
         } else {
             Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-            var math = encoder.matches(user.getPassword(), databaseUser.getPassword());
             if (encoder.matches(user.getPassword(), databaseUser.getPassword())) {
                 return databaseUser;
             }
