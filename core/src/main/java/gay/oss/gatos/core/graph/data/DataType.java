8de0a40f62d2d4969fb0feeedd46ed1c34c4ec9d
williambl <william@williambl.com>
2023-01-31 19:55:03 +0000

    feat: add String datatype

--- core/src/main/java/gay/oss/gatos/core/graph/data/DataType.java
+++ core/src/main/java/gay/oss/gatos/core/graph/data/DataType.java
@@ -9,6 +9,7 @@ package gay.oss.gatos.core.graph.data;
 public record DataType<T>(Class<T> clazz, String name) {
     public static final DataType<Integer> INTEGER = new DataType<>(Integer.class, "integer");
     public static final DataType<Boolean> BOOLEAN = new DataType<>(Boolean.class, "boolean");
+    public static final DataType<String> STRING = new DataType<>(String.class, "string");
 
     /**
      * Creates a new {@link DataBox} with this type and a given value.
