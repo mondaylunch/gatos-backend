5548674d030e1cb1c386587af8eff4dfbe2e0a3a
williambl <william@williambl.com>
2023-01-31 21:49:53 +0000

    feat: add string interpolation node

---
+++ basic_nodes/src/main/java/gay/oss/gatos/basicnodes/BasicNodes.java
@@ -0,0 +1,5 @@
+package gay.oss.gatos.basicnodes;
+
+public final class BasicNodes {
+    public static final StringInterpolationNodeType STRING_INTERPOLATION = new StringInterpolationNodeType();
+}
