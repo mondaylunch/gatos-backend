d80cce8fc700a78ea308cd9dcc91637ce40e69e2
Paul Makles <me@insrt.uk>
2023-02-02 10:02:51 +0000

    test(api): write MVC tests for add user

--- api/src/test/java/gay/oss/gatos/api/controller/test/SignUpControllerTest.java
+++ api/src/test/java/gay/oss/gatos/api/controller/test/SignUpControllerTest.java
@@ -1,84 +1,120 @@
 package gay.oss.gatos.api.controller.test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import gay.oss.gatos.api.UserCreationHelper;
-import gay.oss.gatos.api.controller.SignUpController;
-import gay.oss.gatos.api.exceptions.EmailAlreadyInUseException;
-import gay.oss.gatos.api.exceptions.UsernameAlreadyInUseException;
-import gay.oss.gatos.api.repository.SignUpRepository;
+import gay.oss.gatos.api.BaseMvcTest;
+import gay.oss.gatos.api.helpers.UserCreationHelper;
 import gay.oss.gatos.core.models.User;
 
-public class SignUpControllerTest implements UserCreationHelper {
-    private SignUpRepository signUpRepo = new SignUpRepository();
-    private SignUpController controller = new SignUpController(this.signUpRepo);
-    private static final String KEY = "in_use";
-
-    private UserAddedOutcomes attemptAddUser(User user) {
-        try {
-            this.signUpRepo.addUser(user);
-        } catch (UsernameAlreadyInUseException usernameAlreadyInUseException) {
-            return UserAddedOutcomes.NAME_IN_USE;
-        } catch (EmailAlreadyInUseException emailAlreadyInUseException) {
-            return UserAddedOutcomes.EMAIL_IN_USE;
-        }
-        return UserAddedOutcomes.SUCCESS;
-    }
-
-    private UserAddedOutcomes addDefaultUser() {
-        return this.attemptAddUser(UserCreationHelper.createDefaultUser());
-    }
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SignUpControllerTest extends BaseMvcTest implements UserCreationHelper {
+    private static final String ENDPOINT = "/api/v1/sign_up";
 
     @BeforeEach
     public void setUp() {
-        this.clearUserDatabase();
+        User.objects.clear();
     }
 
     @AfterEach
-    public void resetRepo() {
+    public void tearDown() {
         this.setUp();
     }
 
+    /// --- ADD USER ---
+
     @Test
-    public void canSignUp() {
-        assert this.addDefaultUser() == UserAddedOutcomes.SUCCESS;
+    public void testSignUpSuccess() throws Exception {
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("username", DEFAULT_USERNAME);
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("username", DEFAULT_USERNAME),
+                Map.entry("email", DEFAULT_EMAIL));
     }
 
     @Test
-    public void cannotSignUpWithUsedName() {
-        this.addDefaultUser();
-        assert this.attemptAddUser(UserCreationHelper.createSimpleUser(DEFAULT_USERNAME,
-                "fake@example.com")) == UserAddedOutcomes.NAME_IN_USE;
+    public void testSignUpConflictingEmail() throws Exception {
+        this.createSimpleUser("uniqueusername", DEFAULT_EMAIL);
+
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("username", DEFAULT_USERNAME);
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("error", "Email Already In Use"));
     }
 
     @Test
-    public void cannotSignUpWithUsedEmail() {
-        this.addDefaultUser();
-        assert this.attemptAddUser(
-                UserCreationHelper.createSimpleUser("FakePerson", DEFAULT_EMAIL)) == UserAddedOutcomes.EMAIL_IN_USE;
+    public void testSignUpConflictingUsername() throws Exception {
+        this.createSimpleUser(DEFAULT_USERNAME, "unique@email.com");
+
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("username", DEFAULT_USERNAME);
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("error", "Username Already In Use"));
     }
 
-    // test for the sign-up controller
     @Test
-    public void checkUsernameNotInUse() {
-        var responseBody = this.controller.usernameAlreadyInUse(DEFAULT_USERNAME).getBody();
-        assert responseBody instanceof HashMap<?, ?> bodyMap && bodyMap.get(KEY) instanceof Boolean bool && !bool;
+    public void testSignUpInvalidUsername() throws Exception {
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("username", "a");
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
     @Test
-    public void checkUsernameInUse() {
-        this.addDefaultUser();
-        var responseBody = this.controller.usernameAlreadyInUse(DEFAULT_USERNAME).getBody();
-        assert responseBody instanceof HashMap<?, ?> bodyMap && bodyMap.get(KEY) instanceof Boolean bool && bool;
-    }
+    public void testSignUpInvalidPassword() throws Exception {
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("username", DEFAULT_USERNAME);
+        payload.put("password", "a");
+        String userJson = MAPPER.writeValueAsString(payload);
 
-    private enum UserAddedOutcomes {
-        SUCCESS,
-        NAME_IN_USE,
-        EMAIL_IN_USE
+        this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
