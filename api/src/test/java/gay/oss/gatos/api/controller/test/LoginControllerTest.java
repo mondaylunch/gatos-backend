cb223bb06d4e7f63cb92620a4c7b84e5f9926d64
Paul Makles <me@insrt.uk>
2023-02-02 09:52:26 +0000

    test(api): write MVC tests for login

--- api/src/test/java/gay/oss/gatos/api/controller/test/LoginControllerTest.java
+++ api/src/test/java/gay/oss/gatos/api/controller/test/LoginControllerTest.java
@@ -1,43 +1,91 @@
 package gay.oss.gatos.api.controller.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import gay.oss.gatos.api.UserCreationHelper;
-import gay.oss.gatos.api.controller.LoginController;
-import gay.oss.gatos.api.repository.LoginRepository;
+import gay.oss.gatos.api.BaseMvcTest;
+import gay.oss.gatos.api.helpers.UserCreationHelper;
 import gay.oss.gatos.core.models.User;
 
-public class LoginControllerTest implements UserCreationHelper {
-    private LoginRepository loginRepo = new LoginRepository();
-    private LoginController controller = new LoginController(this.loginRepo);
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest extends BaseMvcTest implements UserCreationHelper {
+    private static final String ENDPOINT = "/api/v1/login";
 
     @BeforeEach
     public void setUp() {
-        this.clearUserDatabase();
+        User.objects.clear();
     }
 
     @AfterEach
-    public void resetRepo() {
+    public void tearDown() {
         this.setUp();
     }
 
+    /// --- AUTHENTICATE ---
+
     @Test
-    public void testFakeUserLoginFailure() {
-        assert this.controller.authenticateUser(new User()).getStatusCode() == HttpStatus.NOT_FOUND;
+    public void testLoginSuccess() throws Exception {
+        this.createDefaultUser();
+
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT + "/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("username", DEFAULT_USERNAME),
+                Map.entry("email", DEFAULT_EMAIL));
     }
 
     @Test
-    public void testLoginSuccess() {
-        var user = UserCreationHelper.createDefaultUser();
-        var databaseUser = UserCreationHelper.createDefaultUser();
-        Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-        String hash = encoder.encode(user.getPassword());
-        databaseUser.setPassword(hash);
-        User.objects.insert(databaseUser);
-        assert this.controller.authenticateUser(user).getStatusCode() == HttpStatus.OK;
+    public void testInvalidEmail() throws Exception {
+        this.createDefaultUser();
+
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", "invalid@example.com");
+        payload.put("password", DEFAULT_PASSWORD);
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT + "/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("error", "User Not Found"));
+    }
+
+    @Test
+    public void testInvalidPassword() throws Exception {
+        this.createDefaultUser();
+
+        Map<String, String> payload = new HashMap<>();
+        payload.put("email", DEFAULT_EMAIL);
+        payload.put("password", "invalid password");
+        String userJson = MAPPER.writeValueAsString(payload);
+
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.post(ENDPOINT + "/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        result = compareFields(OBJECT_EXPRESSION_PREFIX, result,
+                Map.entry("error", "User Not Found"));
     }
 }
