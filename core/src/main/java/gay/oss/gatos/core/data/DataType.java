84f07601681ff662f1007e35aaccb11fc55eb603
williambl <william@williambl.com>
2023-02-03 19:38:38 +0000

    feat: add DataType methods to get optional/list types

--- core/src/main/java/gay/oss/gatos/core/data/DataType.java
+++ core/src/main/java/gay/oss/gatos/core/data/DataType.java
@@ -1,22 +1,82 @@
 package gay.oss.gatos.core.data;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 /**
  * A type of value which can be stored in a {@link DataBox}.
- * @param clazz the class for the data this represents
- * @param name  the name for the data this represents
- * @param <T>   the type of data this represents
  */
-public record DataType<T>(Class<T> clazz, String name) {
-    public static final DataType<Integer> INTEGER = new DataType<>(Integer.class, "integer");
-    public static final DataType<Boolean> BOOLEAN = new DataType<>(Boolean.class, "boolean");
-    public static final DataType<String> STRING = new DataType<>(String.class, "string");
+public final class DataType<T> {
+    public static final DataType<Integer> INTEGER = new DataType<>("integer");
+    public static final DataType<Boolean> BOOLEAN = new DataType<>("boolean");
+    public static final DataType<String> STRING = new DataType<>("string");
+
+    private final String name;
+
+    private DataType<Optional<T>> optionalType = null;
+    private DataType<List<T>> listType = null;
+
+    /**
+     * @param name  the name for the type this represents
+     */
+    public DataType(String name) {
+        this.name = name;
+    }
 
     /**
      * Creates a new {@link DataBox} with this type and a given value.
      * @param value the value
-     * @return      a data box
+     * @return a data box
      */
     public DataBox<T> create(T value) {
         return new DataBox<>(value, this);
+    }
+
+    /**
+     * Returns the name for the type this represents.
+     * @return the name for the type this represents
+     */
+    public String name() {
+        return this.name;
+    }
+
+    /**
+     * Returns the DataType for a list that holds data of this type.
+     * @return the DataType for a list that holds data of this type
+     */
+    public DataType<List<T>> listOf() {
+        if (this.listType == null) {
+            this.listType = new DataType<>("list$" + this.name());
+        }
+        return this.listType;
+    }
+    /**
+     * Returns the DataType for an optional that holds data of this type.
+     * @return the DataType for an optional that holds data of this type
+     */
+    public DataType<Optional<T>> optionalOf() {
+        if (this.optionalType == null) {
+            this.optionalType = new DataType<>("optional$" + this.name());
+        }
+        return this.optionalType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (DataType<?>) obj;
+        return Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "DataType[" + "name=" + this.name + ']';
     }
 }
