4e2a275417cab2545bae342bd2de3179aa9b05b4
williambl <william@williambl.com>
2023-02-07 22:50:32 +0000

    feat: add node type registry

--- basic_nodes/src/main/java/gay/oss/gatos/basicnodes/BasicNodes.java
+++ basic_nodes/src/main/java/gay/oss/gatos/basicnodes/BasicNodes.java
@@ -1,7 +1,9 @@
 package gay.oss.gatos.basicnodes;
 
+import gay.oss.gatos.core.graph.type.NodeTypeRegistry;
+
 public final class BasicNodes {
-    public static final StringInterpolationNodeType STRING_INTERPOLATION = new StringInterpolationNodeType();
-    public static final VariableExtractionNodeType VARIABLE_EXTRACTION = new VariableExtractionNodeType();
-    public static final VariableRemappingNodeType VARIABLE_REMAPPING = new VariableRemappingNodeType();
+    public static final StringInterpolationNodeType STRING_INTERPOLATION = NodeTypeRegistry.register("string_interpolation", new StringInterpolationNodeType());
+    public static final VariableExtractionNodeType VARIABLE_EXTRACTION = NodeTypeRegistry.register("variable_extraction", new VariableExtractionNodeType());
+    public static final VariableRemappingNodeType VARIABLE_REMAPPING = NodeTypeRegistry.register("variable_remapping", new VariableRemappingNodeType());
 }
---
+++ core/src/main/java/gay/oss/gatos/core/graph/type/NodeTypeRegistry.java
@@ -0,0 +1,32 @@
+package gay.oss.gatos.core.graph.type;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Globally stores node types by name.
+ */
+public final class NodeTypeRegistry {
+    private static final Map<String, NodeType> registry = new HashMap<>();
+
+    /**
+     * Registers a given node type to the registry.
+     * @param name  the name of the node type
+     * @param value the node type
+     * @return      the node type
+     */
+    public static <T extends NodeType> T register(String name, T value) {
+        registry.put(name, value);
+        return value;
+    }
+
+    /**
+     * Gets the node type registered under a name.
+     * @param name  the name
+     * @return  an optional of the node type, or empty
+     */
+    public static Optional<NodeType> get(String name) {
+        return Optional.ofNullable(registry.get(name));
+    }
+}
