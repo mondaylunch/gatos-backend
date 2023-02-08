0f4bf1d642877d42b3bb242ba01ebe94a11a2dd8
Paul Makles <me@insrt.uk>
2023-02-08 11:28:56 +0000

    refactor: take advantage of inbuilt error handling

--- api/src/main/java/gay/oss/gatos/api/exceptions/InvalidTokenException.java
+++ api/src/main/java/gay/oss/gatos/api/exceptions/InvalidTokenException.java
@@ -1,16 +1,9 @@
 package gay.oss.gatos.api.exceptions;
 
-import java.util.HashMap;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class InvalidTokenException extends Exception {
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "Invalid Token")
+public class InvalidTokenException extends RuntimeException {
 
-    public static HashMap<String, String> getErrorAsJSON() {
-        HashMap<String, String> error = new HashMap<>();
-        error.put("error", "Invalid Token");
-        return error;
-    }
 }
--- api/src/main/java/gay/oss/gatos/api/exceptions/UserNotFoundException.java
+++ api/src/main/java/gay/oss/gatos/api/exceptions/UserNotFoundException.java
@@ -1,15 +1,9 @@
 package gay.oss.gatos.api.exceptions;
 
-import java.util.HashMap;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class UserNotFoundException extends Exception {
-    public static HashMap<String, String> getErrorAsJSON() {
-        HashMap<String, String> error = new HashMap<>();
-        error.put("error", "User Not Found");
-        return error;
-    }
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User Not Found")
+public class UserNotFoundException extends RuntimeException {
+
 }
