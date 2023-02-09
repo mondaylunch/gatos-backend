f92cd13593a20ea432c5efaa1a4a9b790e453ede
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-02-09 10:35:00 +0000

    feat: create DataType codec

---
+++ core/src/main/java/club/mondaylunch/gatos/codec/DataTypeCodec.java
@@ -0,0 +1,38 @@
+package club.mondaylunch.gatos.codec;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import org.bson.BsonReader;
+import org.bson.BsonWriter;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+
+import club.mondaylunch.gatos.core.data.DataType;
+import club.mondaylunch.gatos.core.data.DataTypeRegistry;
+
+public enum DataTypeCodec implements Codec<DataType<?>> {
+
+    INSTANCE;
+
+    @Override
+    public DataType<?> decode(BsonReader reader, DecoderContext decoderContext) {
+        String dataTypeName = reader.readString();
+        Objects.requireNonNull(dataTypeName, "No data type name found");
+        return DataTypeRegistry.get(dataTypeName)
+            .orElseThrow(() -> new NoSuchElementException("No data type with name " + dataTypeName));
+    }
+
+    @Override
+    public void encode(BsonWriter writer, DataType<?> value, EncoderContext encoderContext) {
+        Objects.requireNonNull(value);
+        writer.writeString(value.name());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Class<DataType<?>> getEncoderClass() {
+        return (Class<DataType<?>>) (Object) DataType.class;
+    }
+}
