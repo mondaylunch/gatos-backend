d9cf94ad89c6dfa8ce06707117376d0c36be0eaa
williambl <william@williambl.com>
2023-02-15 15:25:02 +0000

    feat: add DataTypeConversions

---
+++ core/src/main/java/club/mondaylunch/gatos/core/data/DataTypeConversions.java
@@ -0,0 +1,78 @@
+package club.mondaylunch.gatos.core.data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Manages conversions between {@link DataType DataTypes}.
+ */
+public final class DataTypeConversions {
+    private static final Map<ConversionPair, Function<?, ?>> MAP = new HashMap<>();
+
+    /**
+     * Register a conversion between two types.
+     * @param typeA                 the first DataType
+     * @param typeB                 the second DataType
+     * @param conversionFunction    a function to convert from A to B
+     * @param <A>                   the first type
+     * @param <B>                   the second type
+     */
+    public static <A, B> void register(DataType<A> typeA, DataType<B> typeB, Function<A, B> conversionFunction) {
+        MAP.put(new ConversionPair(typeA, typeB), conversionFunction);
+    }
+
+    /**
+     * Determines whether there is a conversion registered between two DataTypes.
+     * @param a the first DataType
+     * @param b the second DataType
+     * @return  whether there is a conversion between the two
+     */
+    public static boolean canConvert(DataType<?> a, DataType<?> b) {
+        return MAP.containsKey(new ConversionPair(a, b));
+    }
+
+    /**
+     * Convert a DataBox of one type to another.
+     * @param a         the DataBox to convert
+     * @param typeB     the DataType to convert to
+     * @param <A>       the first type
+     * @param <B>       the second type
+     * @return          the converted DataBox
+     */
+    public static <A, B> DataBox<B> convert(DataBox<A> a, DataType<B> typeB) {
+        @SuppressWarnings("unchecked")
+        var func = (Function<A, B>) MAP.get(new ConversionPair(a.type(), typeB));
+        if (func == null) {
+            throw new ConversionException("Cannot convert %s to %s".formatted(a.type(), typeB));
+        }
+        B result = func.apply(a.value());
+        if (result == null) {
+            throw new ConversionException("Conversion function %s -> %s on %s returned null!".formatted(a.type(), typeB, a.value()));
+        }
+
+        return typeB.create(result);
+    }
+
+    private DataTypeConversions() {}
+
+    private record ConversionPair(DataType<?> a, DataType<?> b) {
+    }
+
+    /**
+     * Thrown when there is an error in DataType conversion.
+     */
+    public static class ConversionException extends RuntimeException {
+        public ConversionException(String message) {
+            super(message);
+        }
+
+        public ConversionException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public ConversionException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
