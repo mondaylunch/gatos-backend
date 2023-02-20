d7525b61ae1c9e223aef9df55c2a29b0171aa38c
Pepp <agamer743@gmail.com>
2023-02-20 23:07:53 +0000

    feat: add ParseStringNodeType

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/BasicNodes.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/BasicNodes.java
@@ -15,4 +15,6 @@ public final class BasicNodes {
         .register("string_contains", new StringContainsNodeType());
     public static final StringConcatNodeType STRING_CONCAT = NodeType.REGISTRY
         .register("string_concat", new StringConcatNodeType());
+    public static final ParseStringNodeType PARSE_STRING = NodeType.REGISTRY
+        .register("parse_string", new ParseStringNodeType());
 }
