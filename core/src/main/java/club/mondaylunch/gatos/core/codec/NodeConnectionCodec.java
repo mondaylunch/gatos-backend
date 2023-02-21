a27bfad9b0335ab4f1e8c35cadd6e8434630cc33
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-02-21 17:30:58 +0000

    refactor: use readDocument and writeDocument methods in NodeConnectionCodec

--- core/src/main/java/club/mondaylunch/gatos/core/codec/NodeConnectionCodec.java
+++ core/src/main/java/club/mondaylunch/gatos/core/codec/NodeConnectionCodec.java
@@ -26,23 +26,23 @@ public class NodeConnectionCodec implements Codec<NodeConnection<?>> {
     public NodeConnection<?> decode(BsonReader reader, DecoderContext decoderContext) {
         NodeConnectorCodec outputCodec = new NodeConnectorCodec(this.registry, NodeConnector.Output.class);
         NodeConnectorCodec inputCodec = new NodeConnectorCodec(this.registry, NodeConnector.Input.class);
-        reader.readStartDocument();
-        var output = (NodeConnector.Output<Object>) decoderContext.decodeWithChildContext(outputCodec, reader);
-        var input = (NodeConnector.Input<Object>) decoderContext.decodeWithChildContext(inputCodec, reader);
-        reader.readEndDocument();
-        return new NodeConnection<>(output, input);
+        return SerializationUtils.readDocument(reader, () -> {
+            var output = (NodeConnector.Output<Object>) decoderContext.decodeWithChildContext(outputCodec, reader);
+            var input = (NodeConnector.Input<Object>) decoderContext.decodeWithChildContext(inputCodec, reader);
+            return new NodeConnection<>(output, input);
+        });
     }
 
     @Override
     public void encode(BsonWriter writer, NodeConnection<?> value, EncoderContext encoderContext) {
         NodeConnectorCodec outputCodec = new NodeConnectorCodec(this.registry, NodeConnector.Output.class);
         NodeConnectorCodec inputCodec = new NodeConnectorCodec(this.registry, NodeConnector.Input.class);
-        writer.writeStartDocument();
-        writer.writeName("output");
-        encoderContext.encodeWithChildContext(outputCodec, writer, value.from());
-        writer.writeName("input");
-        encoderContext.encodeWithChildContext(inputCodec, writer, value.to());
-        writer.writeEndDocument();
+        SerializationUtils.writeDocument(writer, () -> {
+            writer.writeName("output");
+            encoderContext.encodeWithChildContext(outputCodec, writer, value.from());
+            writer.writeName("input");
+            encoderContext.encodeWithChildContext(inputCodec, writer, value.to());
+        });
     }
 
     @SuppressWarnings("unchecked")
