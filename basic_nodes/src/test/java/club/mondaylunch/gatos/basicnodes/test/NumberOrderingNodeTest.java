7a4c1851c7f0dc040ce8174df9fc35e2b5b58d06
Pepp <agamer743@gmail.com>
2023-02-21 02:41:44 +0000

    test: add canAddNodeToGraph test

--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/NumberOrderingNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/NumberOrderingNodeTest.java
@@ -12,6 +12,7 @@ import club.mondaylunch.gatos.basicnodes.BasicNodes;
 import club.mondaylunch.gatos.basicnodes.NumberOrderingNodeType;
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
+import club.mondaylunch.gatos.core.graph.Graph;
 import club.mondaylunch.gatos.core.graph.Node;
 
 public class NumberOrderingNodeTest {
@@ -24,6 +25,13 @@ public class NumberOrderingNodeTest {
     private static final Node GTEQ  = Node.create(BasicNodes.NUMBER_ORDERING).modifySetting("mode", NumberOrderingNodeType.NUMBER_ORDERING_MODE.create(NumberOrderingNodeType.Mode.GREATERTHANEQ));
     private static final Node LTEQ  = Node.create(BasicNodes.NUMBER_ORDERING).modifySetting("mode", NumberOrderingNodeType.NUMBER_ORDERING_MODE.create(NumberOrderingNodeType.Mode.LESSTHANEQ));
 
+    @Test
+    public void canAddNodeToGraph() {
+        var graph = new Graph();
+        var node = graph.addNode(BasicNodes.NUMBER_ORDERING);
+        Assertions.assertTrue(graph.containsNode(node));
+    }
+    
     @Test
     public void areInputsCorrect() {
         var node = Node.create(BasicNodes.NUMBER_ORDERING);
