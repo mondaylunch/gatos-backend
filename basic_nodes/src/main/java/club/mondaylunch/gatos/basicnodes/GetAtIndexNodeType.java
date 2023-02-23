0d2ed1c253812600e654e2c1a0309261f25d05b1
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-02-23 23:32:46 +0000

    chore: change the return type

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/GetAtIndexNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/GetAtIndexNodeType.java
@@ -9,12 +9,14 @@ import java.util.concurrent.CompletableFuture;
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
 import club.mondaylunch.gatos.core.data.ListDataType;
+import club.mondaylunch.gatos.core.data.OptionalDataType;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector.Input;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector.Output;
 import club.mondaylunch.gatos.core.graph.type.NodeType;
 
 public class GetAtIndexNodeType extends NodeType.Process {
+    private DataType outType = DataType.ANY;
 
     @Override
     public Map<String, DataBox<?>> settings() {
@@ -31,8 +33,16 @@ public class GetAtIndexNodeType extends NodeType.Process {
 
     @Override
     public Set<Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        var inputType = inputTypes.getOrDefault("in", OptionalDataType.GENERIC_OPTIONAL);
+
+        if (inputType != OptionalDataType.GENERIC_OPTIONAL) {
+            this.outType = inputType;
+        }
+        // do some checks
+        this.outType = DataType.NUMBER;
+
         return Set.of(
-                new NodeConnector.Output<>(nodeId, "output", DataType.ANY));
+            new NodeConnector.Output<>(nodeId, "output", this.outType));
     }
 
     @Override
@@ -40,11 +50,12 @@ public class GetAtIndexNodeType extends NodeType.Process {
         var inputList = DataBox.get(inputs, "input", ListDataType.GENERIC_LIST).orElse(new ArrayList<>());
         Double inputIndex = DataBox.get(inputs, "index", DataType.NUMBER).orElse(0.0);
         int index = inputIndex.intValue();
+        boolean ListOutOfBound = index == inputList.size()-1;
 
-        if (inputList.isEmpty()) {
-            return Map.of("output", CompletableFuture.completedFuture(DataType.ANY.create(null)));
+        if (ListOutOfBound || inputList.isEmpty()) {
+            return Map.of("output", CompletableFuture.completedFuture(this.outType.create(OptionalDataType.GENERIC_OPTIONAL)));
         } else {
-            return Map.of("output", CompletableFuture.completedFuture(DataType.ANY.create(inputList.get(index))));
+            return Map.of("output", CompletableFuture.completedFuture(this.outType.create(inputList.get(index))));
         }
     }
     
