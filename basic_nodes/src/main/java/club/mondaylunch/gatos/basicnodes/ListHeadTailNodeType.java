e300c5af59e3d14ce57e4c183f8c9882bca787bb
“fenn7” <afenn666@gmail.com>
2023-02-24 22:13:20 +0000

    feat: make node determine return type at runtime

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ListHeadTailNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ListHeadTailNodeType.java
@@ -2,65 +2,95 @@ package club.mondaylunch.gatos.basicnodes;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+import com.google.gson.JsonObject;
+
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
 import club.mondaylunch.gatos.core.data.ListDataType;
-import club.mondaylunch.gatos.core.data.OptionalDataType;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
 import club.mondaylunch.gatos.core.graph.type.NodeType;
 
 public class ListHeadTailNodeType extends NodeType.Process {
+    private static final DataType<ExtractionMode> EXTRACTION_MODE_TYPE = DataType.register("extraction_mode_type", ExtractionMode.class);
     @Override
     public Map<String, DataBox<?>> settings() {
         return Map.of(
-            "head_mode", DataType.BOOLEAN.create(true)
+            "extraction_mode", getHeadExtractionBox()
         );
     }
 
     @Override
     public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> state) {
         return Set.of(
-            new NodeConnector.Input<>(nodeId, "input", ListDataType.GENERIC_LIST),
-            new NodeConnector.Input<>(nodeId, "in", OptionalDataType.GENERIC_OPTIONAL)
+            new NodeConnector.Input<>(nodeId, "input", ListDataType.GENERIC_LIST)
         );
     }
 
     @Override
     public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
-        var outputType = inputTypes.getOrDefault("in", OptionalDataType.GENERIC_OPTIONAL);
-        if (outputType == OptionalDataType.GENERIC_OPTIONAL) {
-            outputType = DataType.ANY;
-        } else {
-            outputType = ((OptionalDataType<?>) outputType).contains();
-        }
+        var z = inputTypes;
         return Set.of(
-            new NodeConnector.Output<>(nodeId, "output_first", outputType),
-            new NodeConnector.Output<>(nodeId, "output_rest", ListDataType.GENERIC_LIST)
+            new NodeConnector.Output<>(nodeId, "first", DataType.ANY),
+            new NodeConnector.Output<>(nodeId, "rest", ListDataType.GENERIC_LIST)
         );
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings) {
         var inputList = DataBox.get(inputs, "input", ListDataType.GENERIC_LIST).orElse(List.of());
         if (inputList.isEmpty()) {
             return Map.of(
-                "output_first", CompletableFuture.completedFuture(DataType.ANY.create(null)),
-                "output_rest", CompletableFuture.completedFuture(ListDataType.GENERIC_LIST.create(null))
+                "first", CompletableFuture.completedFuture(DataType.ANY.create(Optional.empty())),
+                "rest", CompletableFuture.completedFuture(ListDataType.GENERIC_LIST.create(List.of()))
             );
         }
-        var shouldExtractHead = DataBox.get(settings, "head_mode", DataType.BOOLEAN).orElse(true);
+        var shouldExtractHead = DataBox.get(settings, "extraction_mode",
+            EXTRACTION_MODE_TYPE).orElse(ExtractionMode.EXTRACT_HEAD) == ExtractionMode.EXTRACT_HEAD;
         var lastIndex = inputList.size() - 1;
         var extractionIndex = shouldExtractHead ? 0 : lastIndex;
         var subListOffset = shouldExtractHead ? 1 : 0;
+        var outputType = this.getTypeFromClass(inputList.get(0).getClass());
         return Map.of(
-            "output_first", CompletableFuture.completedFuture(DataType.ANY.create(
+            "first", CompletableFuture.completedFuture(outputType.create(
                 inputList.get(extractionIndex))),
-            "output_rest", CompletableFuture.completedFuture(ListDataType.GENERIC_LIST.create(
-                inputList.subList(subListOffset, lastIndex + subListOffset)))
+            "rest", CompletableFuture.completedFuture(outputType.listOf().create(
+                (List<Object>) inputList.subList(subListOffset, lastIndex + subListOffset)))
         );
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> DataType<T> getTypeFromClass(Class<?> klass) {
+        DataType type;
+        if (klass == Double.class) {
+            type = DataType.NUMBER;
+        } else if (klass == String.class) {
+            type = DataType.STRING;
+        } else if (klass == Boolean.class) {
+            type = DataType.BOOLEAN;
+        } else if (klass == JsonObject.class) {
+            type = DataType.JSON_OBJECT;
+        } else {
+            type = DataType.ANY;
+        }
+        return type;
+    }
+
+    private enum ExtractionMode {
+        EXTRACT_HEAD,
+        EXTRACT_TAIL
+    }
+
+    public static DataBox<ExtractionMode> getHeadExtractionBox() {
+        return EXTRACTION_MODE_TYPE.create(ExtractionMode.EXTRACT_HEAD);
+    }
+
+    public static DataBox<ExtractionMode> getTailExtractionBox() {
+        return EXTRACTION_MODE_TYPE.create(ExtractionMode.EXTRACT_TAIL);
     }
 }
--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/ListHeadTailNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/ListHeadTailNodeTest.java
@@ -3,7 +3,9 @@ package club.mondaylunch.gatos.basicnodes.test;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import club.mondaylunch.gatos.basicnodes.ListHeadTailNodeType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +23,7 @@ public class ListHeadTailNodeTest {
     @Test
     public void areInputsCorrect() {
         var node = Node.create(BasicNodes.LIST_HEADTAIL);
-        Assertions.assertEquals(2, node.inputs().size());
+        Assertions.assertEquals(1, node.inputs().size());
         Assertions.assertTrue(node.inputs().containsKey("input"));
     }
 
@@ -29,32 +31,31 @@ public class ListHeadTailNodeTest {
     public void areOutputsCorrect() {
         var node = Node.create(BasicNodes.LIST_HEADTAIL);
         Assertions.assertEquals(2, node.getOutputs().size());
-        Assertions.assertTrue(node.getOutputs().containsKey("output_first"));
-        Assertions.assertTrue(node.getOutputs().containsKey("output_rest"));
+        Assertions.assertTrue(node.getOutputs().containsKey("first"));
+        Assertions.assertTrue(node.getOutputs().containsKey("rest"));
     }
 
     @Test
     public void correctlySeparatesInHeadMode() {
-        var node = Node.create(BasicNodes.LIST_HEADTAIL)
-            .modifySetting("head_mode", DataType.BOOLEAN.create(true));
+        var node = Node.create(BasicNodes.LIST_HEADTAIL);
         Map<String, DataBox<?>> input = Map.of(
             "input", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST)
         );
         var output = BasicNodes.LIST_HEADTAIL.compute(input, node.settings());
-        Assertions.assertEquals(TEST_NUM_LIST.get(0), output.get("output_first").join().value());
-        Assertions.assertEquals(TEST_NUM_LIST.subList(1, TEST_NUM_LIST.size()), output.get("output_rest").join().value());
+        Assertions.assertEquals(TEST_NUM_LIST.get(0), output.get("first").join().value());
+        Assertions.assertEquals(TEST_NUM_LIST.subList(1, TEST_NUM_LIST.size()), output.get("rest").join().value());
     }
 
     @Test
     public void correctlySeparatesInTailMode() {
         var node = Node.create(BasicNodes.LIST_HEADTAIL)
-            .modifySetting("head_mode", DataType.BOOLEAN.create(false));
+            .modifySetting("extraction_mode", ListHeadTailNodeType.getTailExtractionBox());
         Map<String, DataBox<?>> input = Map.of(
             "input", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST)
         );
         var output = BasicNodes.LIST_HEADTAIL.compute(input, node.settings());
-        Assertions.assertEquals(TEST_NUM_LIST.get(TEST_NUM_LIST.size() - 1), output.get("output_first").join().value());
-        Assertions.assertEquals(TEST_NUM_LIST.subList(0, TEST_NUM_LIST.size() - 1), output.get("output_rest").join().value());
+        Assertions.assertEquals(TEST_NUM_LIST.get(TEST_NUM_LIST.size() - 1), output.get("first").join().value());
+        Assertions.assertEquals(TEST_NUM_LIST.subList(0, TEST_NUM_LIST.size() - 1), output.get("rest").join().value());
     }
 
     @Test
@@ -64,13 +65,13 @@ public class ListHeadTailNodeTest {
             "input", ListDataType.GENERIC_LIST.create(TEST_STR_LIST)
         );
         var output = BasicNodes.LIST_HEADTAIL.compute(input, node.settings());
-        Assertions.assertEquals(TEST_STR_LIST.get(0), output.get("output_first").join().value());
-        Assertions.assertEquals(List.of(), output.get("output_rest").join().value());
+        Assertions.assertEquals(TEST_STR_LIST.get(0), output.get("first").join().value());
+        Assertions.assertEquals(List.of(), output.get("rest").join().value());
 
-        node.modifySetting("head_mode", DataType.BOOLEAN.create(false));
+        node.modifySetting("extraction_mode", ListHeadTailNodeType.getTailExtractionBox());
         var output1 = BasicNodes.LIST_HEADTAIL.compute(input, node.settings());
-        Assertions.assertEquals(TEST_STR_LIST.get(0), output1.get("output_first").join().value());
-        Assertions.assertEquals(List.of(), output1.get("output_rest").join().value());
+        Assertions.assertEquals(TEST_STR_LIST.get(0), output1.get("first").join().value());
+        Assertions.assertEquals(List.of(), output1.get("rest").join().value());
     }
 
     @Test
@@ -80,7 +81,7 @@ public class ListHeadTailNodeTest {
             "input", ListDataType.GENERIC_LIST.create(TEST_EMPTY_LIST)
         );
         var output = BasicNodes.LIST_HEADTAIL.compute(input, node.settings());
-        Assertions.assertNull(output.get("output_first").join().value());
-        Assertions.assertNull(output.get("output_rest").join().value());
+        Assertions.assertEquals(Optional.empty(), output.get("first").join().value());
+        Assertions.assertEquals(List.of(), output.get("rest").join().value());
     }
 }
