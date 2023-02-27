f6e2d3e2a9cc4ed2c1c8c4fe428964352fe7c6b1
“Serge” <syraj.alkhalil@kcl.ac.uk>
2023-02-27 21:33:52 +0000

    chore: create and send HTTP requests

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/HTTPRequestNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/HTTPRequestNodeType.java
@@ -1,5 +1,13 @@
 package club.mondaylunch.gatos.basicnodes;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -39,7 +47,33 @@ public class HTTPRequestNodeType extends NodeType.Process {
     @Override
     public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
         var url = DataBox.get(settings, "url", DataType.STRING).orElse("");
+        // TODO: make the method impact the request
         var method = DataBox.get(settings, "method", DataType.STRING).orElse("");
+        var body = DataBox.get(inputs, "body", DataType.STRING).orElse("");
+        
+        try {
+            // creating the request
+            HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(url))
+            .POST(BodyPublishers.ofString(body))
+            .build();
+
+            // sending the request
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        boolean notValid = url.equals("") || method.equals("");
+        
+        if (notValid) {
+            return Map.of(
+                "StatusCode", CompletableFuture.completedFuture(DataType.NUMBER.create(404.0)),
+                "responseText", CompletableFuture.completedFuture(DataType.STRING.create("URL or method are incorrect"))
+            );
+        }
 
         return Map.of(
             "StatusCode", CompletableFuture.completedFuture(DataType.NUMBER.create(200.0)),
