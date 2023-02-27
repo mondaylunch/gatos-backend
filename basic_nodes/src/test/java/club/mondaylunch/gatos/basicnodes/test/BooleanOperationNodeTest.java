5bef34f78f2578aa1f70d37aef254a5a4e8ce69f
“fenn7” <afenn666@gmail.com>
2023-02-27 16:51:46 +0000

    fix: compilation errors due to incorrect pull request

--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/BooleanOperationNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/BooleanOperationNodeTest.java
@@ -25,7 +25,7 @@ public class BooleanOperationNodeTest {
     public void canAddNodeToGraph() {
         var graph = new Graph();
         var node = graph.addNode(BasicNodes.BOOL_OP);
-        Assertions.assertTrue(graph.containsNode(node)); 
+        Assertions.assertTrue(graph.containsNode(node));
     }
 
     @Test
@@ -33,23 +33,23 @@ public class BooleanOperationNodeTest {
         Map<String, DataBox<?>> inputs = Map.of(
             "input", DataType.BOOLEAN.create(true)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, NOT.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, NOT.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
 
         inputs = Map.of(
             "input", DataType.BOOLEAN.create(false)
         );
-        result = (boolean) BasicNodes.BOOL_OP.compute(inputs, NOT.settings()).get("output").join().value();
+        result = (boolean) BasicNodes.BOOL_OP.compute(inputs, NOT.settings(), Map.of()).get("output").join().value();
         assertTrue(result);
     }
 
     @Test
-    public void correctlyResolvesOrDiff() {        
+    public void correctlyResolvesOrDiff() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(false)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings(), Map.of()).get("output").join().value();
         assertTrue(result);
     }
 
@@ -59,7 +59,7 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(true)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings(), Map.of()).get("output").join().value();
         assertTrue(result);
     }
 
@@ -69,18 +69,18 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(false),
                 "inputB", DataType.BOOLEAN.create(false)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, OR.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
     }
 
     @Test
-    public void correctlyResolvesAndDiff() {        
+    public void correctlyResolvesAndDiff() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(false)
         );
 
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
     }
 
@@ -90,7 +90,7 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(true)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings(), Map.of()).get("output").join().value();
         assertTrue(result);
     }
 
@@ -100,17 +100,17 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(false),
                 "inputB", DataType.BOOLEAN.create(false)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, AND.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
     }
 
     @Test
-    public void correctlyResolvesXorDiff() {        
+    public void correctlyResolvesXorDiff() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(false)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings(), Map.of()).get("output").join().value();
         assertTrue(result);
     }
 
@@ -120,7 +120,7 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(true),
                 "inputB", DataType.BOOLEAN.create(true)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
     }
 
@@ -130,7 +130,7 @@ public class BooleanOperationNodeTest {
                 "inputA", DataType.BOOLEAN.create(false),
                 "inputB", DataType.BOOLEAN.create(false)
         );
-        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings()).get("output").join().value();
+        boolean result = (boolean) BasicNodes.BOOL_OP.compute(inputs, XOR.settings(), Map.of()).get("output").join().value();
         assertFalse(result);
     }
 }
--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/MathNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/MathNodeTest.java
@@ -41,159 +41,159 @@ public class MathNodeTest {
     }
 
     @Test
-    public void integerAddition() {        
+    public void integerAddition() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, ADD.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, ADD.settings(), Map.of()).get("output").join().value();
         assertEquals(25, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) -15)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, ADD.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, ADD.settings(), Map.of()).get("output").join().value();
         assertEquals(-5, result);
     }
 
     @Test
-    public void integerSubtraction() {        
+    public void integerSubtraction() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, SUB.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, SUB.settings(), Map.of()).get("output").join().value();
         assertEquals(-5, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) -15)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, SUB.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, SUB.settings(), Map.of()).get("output").join().value();
         assertEquals(25, result);
     }
 
     @Test
-    public void integerMultiplication() {        
+    public void integerMultiplication() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, MLT.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, MLT.settings(), Map.of()).get("output").join().value();
         assertEquals(150, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) -15)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, MLT.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, MLT.settings(), Map.of()).get("output").join().value();
         assertEquals(-150, result);
     }
 
     @Test
-    public void integerDivision() {        
+    public void integerDivision() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 90),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(6, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) -10),
                 "inputB", DataType.NUMBER.create( (double) 2)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(-5, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) 20)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(0.5, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 10),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals((double) 2 / 3, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( (double) 15),
                 "inputB", DataType.NUMBER.create( (double) 10)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(1.5, result);
     }
 
     @Test
-    public void doubleAddition() {        
+    public void doubleAddition() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 10.25),
                 "inputB", DataType.NUMBER.create( 9.75)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, ADD.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, ADD.settings(), Map.of()).get("output").join().value();
         assertEquals(20, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 9.75),
                 "inputB", DataType.NUMBER.create( -8.25)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, ADD.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, ADD.settings(), Map.of()).get("output").join().value();
         assertEquals(1.5, result);
     }
 
     @Test
-    public void doubleSubtraction() {        
+    public void doubleSubtraction() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create(11.45),
                 "inputB", DataType.NUMBER.create(0.25)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, SUB.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, SUB.settings(), Map.of()).get("output").join().value();
         assertEquals(11.2, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create(100.001),
                 "inputB", DataType.NUMBER.create(-99.999)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, SUB.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, SUB.settings(), Map.of()).get("output").join().value();
         assertEquals(200, result);
     }
 
     @Test
-    public void doubleMultiplication() {        
+    public void doubleMultiplication() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 0.1),
                 "inputB", DataType.NUMBER.create( (double) 15)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, MLT.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, MLT.settings(), Map.of()).get("output").join().value();
         assertEquals(1.5, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 0.25),
                 "inputB", DataType.NUMBER.create( 3.1)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, MLT.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, MLT.settings(), Map.of()).get("output").join().value();
         assertEquals(0.775, result);
     }
 
     @Test
-    public void doubleDivision() {        
+    public void doubleDivision() {
         Map<String, DataBox<?>> inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 1.5),
                 "inputB", DataType.NUMBER.create( 0.75)
         );
-        double result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        double result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(2, result);
 
         inputs = Map.of(
                 "inputA", DataType.NUMBER.create( 74.213),
                 "inputB", DataType.NUMBER.create( 2.4)
         );
-        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings()).get("output").join().value();
+        result = (double) BasicNodes.MATH.compute(inputs, DIV.settings(), Map.of()).get("output").join().value();
         assertEquals(30.9220833333333333, result);
     }
 }
