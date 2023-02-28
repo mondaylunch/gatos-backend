b6256f6413f02ab41848947eccabf554e9b313cf
Pepp <agamer743@gmail.com>
2023-02-28 16:54:17 +0000

    fix: change params to work with changes to NodeType

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/NumberComparisonNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/NumberComparisonNodeType.java
@@ -20,7 +20,7 @@ public class NumberComparisonNodeType extends NodeType.Process {
     }
 
     @Override
-    public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> settings) {
+    public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
         return Set.of(
             new NodeConnector.Input<>(nodeId, "inputA", DataType.NUMBER),
             new NodeConnector.Input<>(nodeId, "inputB", DataType.NUMBER)
@@ -28,13 +28,13 @@ public class NumberComparisonNodeType extends NodeType.Process {
     }
 
     @Override
-    public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings) {
+    public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
         return Set.of(
             new NodeConnector.Output<>(nodeId, "output", DataType.BOOLEAN));
     }
 
     @Override
-    public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings) {
+    public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
         return Map.of("output", CompletableFuture.completedFuture(DataType.BOOLEAN.create(
             DataBox.get(settings, "mode", NUMBER_ORDERING_MODE).orElseThrow().apply(
                 DataBox.get(inputs, "inputA", DataType.NUMBER).orElseThrow(),
@@ -61,5 +61,18 @@ public class NumberComparisonNodeType extends NodeType.Process {
         }
 
         public abstract boolean apply(double a, double b);
+    }
+
+    public Map<String, CompletableFuture<DataBox<?>>> compute (Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings) {
+        return compute(inputs, settings, Map.of());
+    }
+ 
+    /**
+     * If no mode is given default to greaterthan
+     * @param inputs    a map of the two given doubles
+     * @return          a map of the computed result
+     */
+    public Map<String, CompletableFuture<DataBox<?>>> compute (Map<String, DataBox<?>> inputs) {
+        return compute(inputs, settings(), Map.of());
     }
 }
