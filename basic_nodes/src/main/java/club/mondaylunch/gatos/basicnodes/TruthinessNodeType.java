360f4dc3352b7dae2bcce7065c60ab3ad80d71c2
“fenn7” <afenn666@gmail.com>
2023-03-02 15:16:44 +0000

    refactor: rewrite truthiness node functionality, add tests

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/TruthinessNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/TruthinessNodeType.java
@@ -16,7 +16,7 @@ import club.mondaylunch.gatos.core.graph.type.NodeType;
 public class TruthinessNodeType extends NodeType.Process {
     @Override
     public Map<String, DataBox<?>> settings() {
-        return null;
+        return Map.of();
     }
 
     @Override
@@ -34,22 +34,33 @@ public class TruthinessNodeType extends NodeType.Process {
     }
 
     @Override
-    @SuppressWarnings("ALL")
     public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
         var inputType = inputTypes.getOrDefault("input", DataType.ANY);
-        var input = DataBox.get(inputs, "input_data", DataType.ANY);
-        return Map.of("output", CompletableFuture.completedFuture(DataType.BOOLEAN.create(input.isPresent() && this.isInherentlyTruthy(inputType, input)
-            || switch (inputType.name()) {
-                case "number" -> (double) input.get() != 0.0D;
-                case "boolean" -> (boolean) input.get();
-                case "string" -> ((String) input.get()).length() != 0;
-                default -> false;
-            })));
+        // NB: "OptionalDataType.GENERIC_OPTIONAL instanceof OptionalDataType" is false
+        var input = inputType instanceof OptionalDataType<?>
+            ? (Optional<?>) DataBox.get(inputs, "input_data", inputType).orElseThrow()
+            : DataBox.get(inputs, "input_data", inputType);
+        return Map.of("output", CompletableFuture.completedFuture(
+            DataType.BOOLEAN.create(isInherentlyTruthy(inputType) || input.map(value -> evaluateValue(evaluateExactType(inputType), value)).orElse(false))));
     }
 
-    private boolean isInherentlyTruthy(DataType<?> dataType, Object input) {
-        return dataType instanceof ListDataType<?>
-            || (dataType instanceof OptionalDataType<?> && ((Optional<?>) input).isPresent())
-            || dataType.equals(DataType.JSON_OBJECT);
+    private static DataType<?> evaluateExactType(DataType<?> dataType) {
+        return dataType instanceof OptionalDataType<?> opt
+            ? opt.contains()
+            : dataType;
+    }
+
+    private static boolean isInherentlyTruthy(DataType<?> dataType) {
+        // NB: "ListDataType.GENERIC_LIST instanceof ListDataType" is false
+        return dataType instanceof ListDataType<?> || dataType.equals(DataType.JSON_OBJECT);
+    }
+
+    private static boolean evaluateValue(DataType<?> dataType, Object value) {
+        return switch (dataType.name()) {
+            case "number" -> (double) value != 0;
+            case "boolean" -> (boolean) value;
+            case "string" -> ((String) value).length() != 0;
+            default -> false;
+        };
     }
 }
