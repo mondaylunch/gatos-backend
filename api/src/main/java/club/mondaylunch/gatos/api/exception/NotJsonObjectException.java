8d8603e6deb3fa336ef2fdda4b1b1ccf1689920a
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2023-03-06 22:55:28 +0000

    refactor: remove unused exception

--- api/src/main/java/club/mondaylunch/gatos/api/exception/NotJsonObjectException.java
+++
@@ -1,8 +0,0 @@
-package club.mondaylunch.gatos.api.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Body isn't a JSON object")
-public class NotJsonObjectException extends RuntimeException {
-}
