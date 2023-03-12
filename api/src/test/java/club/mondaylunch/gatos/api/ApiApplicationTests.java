e61eb7b0e82dedebb13683f321b138b8b97aad0e
williambl <william@williambl.com>
2023-03-12 12:50:29 +0000

    test: remove ApiApplicationTests componentscan annotation

--- api/src/test/java/club/mondaylunch/gatos/api/ApiApplicationTests.java
+++ api/src/test/java/club/mondaylunch/gatos/api/ApiApplicationTests.java
@@ -2,15 +2,8 @@ package club.mondaylunch.gatos.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-
-import club.mondaylunch.gatos.api.auth.SecurityConfig;
 
 @SpringBootTest
-@ComponentScan(basePackages = "club.mondaylunch.gatos.api", excludeFilters = {
-    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = { SecurityConfig.class })
-})
 class ApiApplicationTests {
 
     @Test
