3c2cf73b9c2c6bd89c32bbf9f2d1e3b7bbebb9e2
williambl <william@williambl.com>
2023-03-12 17:24:34 +0000

    fix: discord data types not being loaded

--- discord/src/main/java/club/mondaylunch/gatos/discord/DiscordDataTypes.java
+++ discord/src/main/java/club/mondaylunch/gatos/discord/DiscordDataTypes.java
@@ -6,4 +6,8 @@ public class DiscordDataTypes {
     public static final DataType<String> GUILD_ID = DataType.register("discord.guild_id", String.class);
     public static final DataType<String> CHANNEL_ID = DataType.register("discord.channel_id", String.class);
     public static final DataType<String> USER_ID = DataType.register("discord.user_id", String.class);
+
+    static void init() {
+
+    }
 }
