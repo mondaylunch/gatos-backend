f1c768b2dc9be92dc5d91305c0b8b25347417a39
williambl <william@williambl.com>
2023-03-12 18:38:40 +0000

    feat: discord command node type

---
+++ discord/src/main/java/club/mondaylunch/gatos/discord/DiscordCommandNodeType.java
@@ -0,0 +1,77 @@
+package club.mondaylunch.gatos.discord;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.Nullable;
+
+import club.mondaylunch.gatos.core.data.DataBox;
+import club.mondaylunch.gatos.core.data.DataType;
+import club.mondaylunch.gatos.core.graph.Node;
+import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
+import club.mondaylunch.gatos.core.graph.type.NodeType;
+import club.mondaylunch.gatos.core.models.Flow;
+
+public class DiscordCommandNodeType extends NodeType.Start<SlashCommandInteractionEvent> {
+    private final GatosDiscord gatosDiscord;
+
+    public DiscordCommandNodeType(GatosDiscord gatosDiscord) {
+        this.gatosDiscord = gatosDiscord;
+    }
+
+    @Override
+    public Map<String, DataBox<?>> settings() {
+        return Map.of(
+            "guild_id", DiscordDataTypes.GUILD_ID.create(""),
+            "command_name", DataType.STRING.create("")
+        );
+    }
+
+    @Override
+    public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        return Set.of(
+            new NodeConnector.Output<>(nodeId, "user", DiscordDataTypes.USER_ID),
+            new NodeConnector.Output<>(nodeId, "channel", DiscordDataTypes.CHANNEL_ID)
+        );
+    }
+
+    @Override
+    public void setupFlow(Flow flow, Consumer<@Nullable SlashCommandInteractionEvent> function, Node node) {
+        String guildId = DataBox.get(node.settings(), "guild_id", DiscordDataTypes.GUILD_ID).orElseThrow();
+        String commandName = DataBox.get(node.settings(), "command_name", DataType.STRING).orElseThrow();
+        Guild guild = this.gatosDiscord.getJda().getGuildById(guildId);
+        if (guild == null) {
+            throw new IllegalStateException("Guild not found: " + guildId);
+        }
+        this.gatosDiscord.createSlashCommandListener(node.id(), commandName, guild, s -> {
+            if (s != null) {
+                s.reply("...").setEphemeral(true).queue();
+            }
+            function.accept(s);
+        });
+    }
+
+    @Override
+    public void teardownFlow(Flow flow, Node node) {
+        this.gatosDiscord.removeSlashCommandListener(node.id());
+    }
+
+    @Override
+    public Map<String, CompletableFuture<DataBox<?>>> compute(@Nullable SlashCommandInteractionEvent event, Map<String, DataBox<?>> settings) {
+        if (event == null) {
+            return Map.of(
+                "user", CompletableFuture.completedFuture(DiscordDataTypes.USER_ID.create("")),
+                "channel", CompletableFuture.completedFuture(DiscordDataTypes.CHANNEL_ID.create(""))
+            );
+        }
+        return Map.of(
+            "user", CompletableFuture.completedFuture(DiscordDataTypes.USER_ID.create(event.getUser().getId())),
+            "channel", CompletableFuture.completedFuture(DiscordDataTypes.CHANNEL_ID.create(event.getChannel().getId()))
+        );
+    }
+}
--- discord/src/main/java/club/mondaylunch/gatos/discord/GatosDiscord.java
+++ discord/src/main/java/club/mondaylunch/gatos/discord/GatosDiscord.java
@@ -2,11 +2,16 @@ package club.mondaylunch.gatos.discord;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +19,9 @@ import club.mondaylunch.gatos.core.GatosPlugin;
 import club.mondaylunch.gatos.core.graph.type.NodeType;
 
 public class GatosDiscord implements GatosPlugin {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GatosDiscord.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger("Gatos Discord");
     private JDA jda;
+    private GatosCommands commands;
 
     @Override
     public void init() {
@@ -24,11 +30,14 @@ public class GatosDiscord implements GatosPlugin {
             this.jda.shutdown();
         }
 
+        this.commands = new GatosCommands(this);
+
         try {
             LOGGER.info("Constructing new JDA instance");
             this.jda = JDABuilder.createDefault(this.getToken())
                 .setActivity(Activity.competing("SEG"))
                 .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(this.commands)
                 .build()
                 .awaitReady();
             LOGGER.info("JDA ready");
@@ -36,8 +45,11 @@ public class GatosDiscord implements GatosPlugin {
             throw new RuntimeException("Failed to set up JDA", e);
         }
 
+        this.jda.getGuilds().forEach(g -> g.updateCommands().queue());
+
         DiscordDataTypes.init();
         NodeType.REGISTRY.register("discord.send_message", new SendDiscordMessageNode(() -> this.jda));
+        NodeType.REGISTRY.register("discord.command", new DiscordCommandNodeType(this));
     }
 
     private String getToken() {
@@ -48,5 +60,17 @@ public class GatosDiscord implements GatosPlugin {
             throw new RuntimeException("Failure loading discord token", e);
         }
         return properties.getProperty("DISCORD_TOKEN");
+    }
+
+    public JDA getJda() {
+        return this.jda;
+    }
+
+    public void createSlashCommandListener(UUID id, String commandName, Guild guild, Consumer<@Nullable SlashCommandInteractionEvent> function) {
+        this.commands.createSlashCommandListener(id, commandName, guild, function);
+    }
+
+    public void removeSlashCommandListener(UUID id) {
+        this.commands.removeSlashCommandListener(id);
     }
 }
