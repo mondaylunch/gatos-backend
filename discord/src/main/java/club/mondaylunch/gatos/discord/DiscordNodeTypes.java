703ac55f3e9998543e9d151fbb602fdafaaf68b1
williambl <william@williambl.com>
2023-03-15 14:25:00 +0000

    feat: add Users With Role nodetype

--- discord/src/main/java/club/mondaylunch/gatos/discord/DiscordNodeTypes.java
+++ discord/src/main/java/club/mondaylunch/gatos/discord/DiscordNodeTypes.java
@@ -5,18 +5,21 @@ import club.mondaylunch.gatos.discord.nodes.ApplyRoleNodeType;
 import club.mondaylunch.gatos.discord.nodes.CommandReplyNodeType;
 import club.mondaylunch.gatos.discord.nodes.ReceiveCommandNodeType;
 import club.mondaylunch.gatos.discord.nodes.SendMessageNodeType;
+import club.mondaylunch.gatos.discord.nodes.UsersWithRoleNodeType;
 
 public class DiscordNodeTypes {
     private final ReceiveCommandNodeType receiveCommandNodeType;
     private final CommandReplyNodeType commandReplyNodeType;
     private final SendMessageNodeType sendMessageNodeTypeType;
     private final ApplyRoleNodeType applyRoleNodeType;
+    private final UsersWithRoleNodeType usersWithRoleNodeType;
 
     public DiscordNodeTypes(GatosDiscord gatosDiscord) {
         this.receiveCommandNodeType = NodeType.REGISTRY.register("discord.receive_command", new ReceiveCommandNodeType(gatosDiscord));
         this.commandReplyNodeType = NodeType.REGISTRY.register("discord.reply_to_command", new CommandReplyNodeType(gatosDiscord));
         this.sendMessageNodeTypeType = NodeType.REGISTRY.register("discord.send_message", new SendMessageNodeType(gatosDiscord::getJda));
         this.applyRoleNodeType = NodeType.REGISTRY.register("discord.apply_role", new ApplyRoleNodeType(gatosDiscord::getJda));
+        this.usersWithRoleNodeType = NodeType.REGISTRY.register("discord.users_with_role", new UsersWithRoleNodeType(gatosDiscord));
     }
 
     public SendMessageNodeType sendDiscordMessage() {
@@ -33,5 +36,9 @@ public class DiscordNodeTypes {
 
     public ApplyRoleNodeType applyRole() {
         return this.applyRoleNodeType;
+    }
+
+    public UsersWithRoleNodeType usersWithRole() {
+        return this.usersWithRoleNodeType;
     }
 }
