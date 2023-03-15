08a4115643260ac0fefcddd7bfbefcacda72a27c
“fenn7” <afenn666@gmail.com>
2023-03-15 15:36:56 +0000

    style: unused impor

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ListSortNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ListSortNodeType.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
