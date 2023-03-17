443d9e588a7c925fd2185a78cd3a5c5f2fb81336
Serge Alkhalil <syraj.alkhalil@kcl.ac.uk>
2023-03-17 21:19:37 +0000

    test: add more tests for create json node

--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/CreateJSONNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/CreateJSONNodeTest.java
@@ -1,8 +1,11 @@
 package club.mondaylunch.gatos.basicnodes.test;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.google.gson.JsonObject;
 import club.mondaylunch.gatos.basicnodes.BasicNodes;
 import club.mondaylunch.gatos.core.graph.Node;
 
@@ -22,4 +25,9 @@ public class CreateJSONNodeTest {
         Assertions.assertTrue(node.getOutputs().containsKey("output"));
     }
 
+    @Test
+    public void identifiesEqualStrings() {
+        var output = BasicNodes.CREATE_JSON.compute(Map.of(), Map.of(), Map.of());
+        Assertions.assertEquals(new JsonObject(), output.get("output").join().value());
+    }
 }
