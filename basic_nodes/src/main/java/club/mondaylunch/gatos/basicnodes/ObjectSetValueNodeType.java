aaa21a2522b6a70eb9487893bd90a950c7eeb065
Serge Alkhalil <syraj.alkhalil@kcl.ac.uk>
2023-03-17 22:31:56 +0000

    feat: add implementation for object set value node

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ObjectSetValueNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ObjectSetValueNodeType.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
@@ -38,8 +41,17 @@ public class ObjectSetValueNodeType extends NodeType.Process {
 
     @Override
     public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compute'");
+        var key = (String) settings.get("key").value();
+        var jsonObject = DataBox.get(inputs, "object", DataType.JSON_OBJECT).orElse(new JsonObject());
+        var jsonElement = DataBox.get(inputs, "element", DataType.JSON_ELEMENT);
+
+        // the add method will update a value if it exists or add a new value if a key doesn't exist
+        // constructor is deprecated for json element so we will check if the json element is empty or not
+        if (!("".equals(key)) && jsonElement.isPresent()) {
+            jsonObject.add(key, jsonElement.get());
+        }
+        
+        return Map.of("output", CompletableFuture.completedFuture(DataType.JSON_OBJECT.create(jsonObject)));
     }
     
 }
