da644f53b7b5b42e5e823e45c3a7a3c1fd232849
“fenn7” <afenn666@gmail.com>
2023-03-20 22:40:46 +0000

    fix: remove all unnecessary file changes

---
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/ListReverseNodeType.java
@@ -0,0 +1,50 @@
+package club.mondaylunch.gatos.basicnodes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import club.mondaylunch.gatos.core.data.DataBox;
+import club.mondaylunch.gatos.core.data.DataType;
+import club.mondaylunch.gatos.core.data.ListDataType;
+import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
+import club.mondaylunch.gatos.core.graph.type.NodeType;
+
+public class ListReverseNodeType extends NodeType.Process {
+    @Override
+    public Map<String, DataBox<?>> settings() {
+        return Map.of();
+    }
+
+    @Override
+    public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        return Set.of(
+            new NodeConnector.Input<>(nodeId, "input", ListDataType.GENERIC_LIST));
+    }
+
+    @Override
+    public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        var outputType = inputTypes.getOrDefault("input", ListDataType.GENERIC_LIST);
+        return Set.of(
+            new NodeConnector.Output<>(nodeId, "output", outputType));
+    }
+
+    @Override
+    public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        var inputListCopy = new ArrayList<>(DataBox.get(inputs, "input", ListDataType.GENERIC_LIST).orElseThrow());
+        var outputType = inputTypes.getOrDefault("input", ListDataType.GENERIC_LIST);
+        Collections.reverse(inputListCopy);
+        return Map.of("output", CompletableFuture.completedFuture(
+            this.getGenericListBox(inputListCopy.stream().toList(), outputType))
+        );
+    }
+
+    @SuppressWarnings({"unchecked", "ListUsedAsFieldOrParameterType"})
+    private <T> DataBox<T> getGenericListBox(List<?> list, DataType<?> type) {
+        return ((DataType<T>) type).create((T) list);
+    }
+}
---
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/ListContainsNodeTest.java
@@ -0,0 +1,64 @@
+package club.mondaylunch.gatos.basicnodes.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import club.mondaylunch.gatos.basicnodes.BasicNodes;
+import club.mondaylunch.gatos.core.data.DataBox;
+import club.mondaylunch.gatos.core.data.DataType;
+import club.mondaylunch.gatos.core.data.ListDataType;
+import club.mondaylunch.gatos.core.graph.Node;
+
+public class ListContainsNodeTest {
+    private static final List<Double> TEST_NUM_LIST = Arrays.asList(1., 3., 7., 21.);
+    private static final List<String> TEST_STR_LIST = Arrays.asList("z", "y", "x", "w", "v");
+
+    @Test
+    public void areInputsCorrect() {
+        var node = Node.create(BasicNodes.LIST_CONTAINS);
+        Assertions.assertEquals(2, node.inputs().size());
+        Assertions.assertTrue(node.inputs().containsKey("list"));
+        Assertions.assertTrue(node.inputs().containsKey("element"));
+    }
+
+    @Test
+    public void areOutputsCorrect() {
+        var node = Node.create(BasicNodes.LIST_CONTAINS);
+        Assertions.assertEquals(1, node.getOutputs().size());
+        Assertions.assertTrue(node.getOutputs().containsKey("output"));
+    }
+
+    @Test
+    public void correctlyFindsIfElementPresent() {
+        var node = Node.create(BasicNodes.LIST_CONTAINS);
+        Map<String, DataBox<?>> dataInputs = Map.of(
+            "list", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST),
+            "element", DataType.ANY.create(21.)
+        );
+        Map<String, DataType<?>> typeInputs = Map.of(
+            "list", DataType.NUMBER.listOf(),
+            "element", DataType.NUMBER
+        );
+        var output = BasicNodes.LIST_CONTAINS.compute(dataInputs, node.settings(), typeInputs);
+        Assertions.assertTrue((boolean) output.get("output").join().value());
+    }
+
+    @Test
+    public void correctlyFindsIfElementAbsent() {
+        var node = Node.create(BasicNodes.LIST_CONTAINS);
+        Map<String, DataBox<?>> dataInputs = Map.of(
+            "list", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST),
+            "element", DataType.ANY.create(69.)
+        );
+        Map<String, DataType<?>> typeInputs = Map.of(
+            "list", DataType.NUMBER.listOf(),
+            "element", DataType.NUMBER
+        );
+        var output = BasicNodes.LIST_CONTAINS.compute(dataInputs, node.settings(), typeInputs);
+        Assertions.assertFalse((boolean) output.get("output").join().value());
+    }
+}
