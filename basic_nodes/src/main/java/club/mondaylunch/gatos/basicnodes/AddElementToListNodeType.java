990a29cfdde81bfed4a30e2fd738e5279b9c17a6
Pepp <agamer743@gmail.com>
2023-03-22 20:31:05 +0000

    fix: correctly handles incompatible types

--- basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/AddElementToListNodeType.java
+++ basic_nodes/src/main/java/club/mondaylunch/gatos/basicnodes/AddElementToListNodeType.java
@@ -6,12 +6,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.Optional;
 
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
 import club.mondaylunch.gatos.core.data.ListDataType;
-import club.mondaylunch.gatos.core.data.OptionalDataType;
 import club.mondaylunch.gatos.core.graph.connector.NodeConnector;
 import club.mondaylunch.gatos.core.graph.type.NodeType;
 
@@ -24,32 +22,35 @@ public class AddElementToListNodeType extends NodeType.Process {
 
     @Override
     public Set<NodeConnector.Input<?>> inputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
+        var inputListType = this.getExactListType(inputTypes.getOrDefault("list", ListDataType.GENERIC_LIST));
+        var inputElemType = this.getExactElemType(inputListType);
+
         return Set.of(
-            new NodeConnector.Input<>(nodeId, "list", this.getList(inputTypes)),
-            new NodeConnector.Input<>(nodeId, "element", this.getElement(inputTypes))
+            new NodeConnector.Input<>(nodeId, "list", inputListType),
+            new NodeConnector.Input<>(nodeId, "element", inputElemType)
         );
     }
 
     @Override
     public Set<NodeConnector.Output<?>> outputs(UUID nodeId, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
-        DataType<?> outType = this.getList(inputTypes);
+        var output = this.getExactListType(inputTypes.getOrDefault("list", ListDataType.GENERIC_LIST));
         
         return Set.of(
-            new NodeConnector.Output<>(nodeId, "output", outType)
+            new NodeConnector.Output<>(nodeId, "output", output)
         );
     }
 
     @Override
     public Map<String, CompletableFuture<DataBox<?>>> compute(Map<String, DataBox<?>> inputs, Map<String, DataBox<?>> settings, Map<String, DataType<?>> inputTypes) {
-        var inputElem = DataBox.get(inputs, "element", DataType.ANY).orElseThrow();
-        var inputList = DataBox.get(inputs, "list", ListDataType.GENERIC_LIST).orElseThrow();
+        var inputListType = this.getExactListType(inputTypes.getOrDefault("list", ListDataType.GENERIC_LIST));
+        var inputElemType = this.getExactElemType(inputListType);
+        var inputList = (List<?>) DataBox.get(inputs, "list", inputListType).orElseThrow();
+        var inputElem = DataBox.get(inputs, "element", inputElemType).orElseThrow();
 
-        DataType<?> elemType = this.getElement(inputTypes);
-
-        List<?> typedList = this.generateTypeList(elemType, inputList, inputElem);
-
+        var copyList = new ArrayList<>(inputList);
+        var output = generateList(copyList, inputElem);
         return Map.of(
-            "output", CompletableFuture.completedFuture(this.getGenericListBox(typedList, this.getList(inputTypes)))
+            "output", CompletableFuture.completedFuture(this.getGenericListBox(output, inputListType))
         );
     }
 
@@ -58,39 +59,16 @@ public class AddElementToListNodeType extends NodeType.Process {
         return ((DataType<T>) type).create((T) list);
     }
 
-    private DataType<?>[] getTypesFromInputs(Map<String, DataType<?>> inputTypes) {
-        DataType<?> elemType = inputTypes.getOrDefault("element", DataType.ANY);
-        DataType<?> listRefType = inputTypes.getOrDefault("list", ListDataType.GENERIC_LIST);
-        
-        DataType<?> listType;
-        if (elemType == DataType.ANY) {
-            if (listRefType == ListDataType.GENERIC_LIST) {
-                listType = ListDataType.GENERIC_LIST;
-            }
-            else {
-                listType = listRefType;
-                elemType = ((ListDataType<?>) listRefType).contains();
-            }
-        }
-        else listType = elemType.listOf();
-
-        DataType<?>[] out = new DataType<?>[4];
-        out[0] = listType;
-        out[1] = elemType;
-
-        return out;
+    private DataType<?> getExactListType(DataType<?> type) {
+        return type instanceof ListDataType<?> list ? list : ListDataType.GENERIC_LIST;
     }
 
-    private DataType<?> getList(Map<String, DataType<?>> inputTypes) {
-        return getTypesFromInputs(inputTypes)[0];
-    }
-    private DataType<?> getElement(Map<String, DataType<?>> inputTypes) {
-        return getTypesFromInputs(inputTypes)[1];
+    private DataType<?> getExactElemType(DataType<?> type) {
+        return type instanceof ListDataType<?> list ? list.contains() : DataType.ANY;
     }
 
-    private <T> List<T> generateTypeList(DataType<T> type, List<?> initialize, Object element) {
-        List<T> output = new ArrayList<T>((List<T>) initialize);
-        output.add((T) element);
-        return output;
+    private <T> List<?> generateList(ArrayList<T> list, Object element) {
+        list.add((T) element);
+        return list;
     }
 }
--- basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/AddElementToListNodeTest.java
+++ basic_nodes/src/test/java/club/mondaylunch/gatos/basicnodes/test/AddElementToListNodeTest.java
@@ -3,6 +3,7 @@ package club.mondaylunch.gatos.basicnodes.test;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,11 @@ import org.junit.jupiter.api.Test;
 import club.mondaylunch.gatos.basicnodes.BasicNodes;
 import club.mondaylunch.gatos.core.data.DataBox;
 import club.mondaylunch.gatos.core.data.DataType;
-import club.mondaylunch.gatos.core.data.ListDataType;
 import club.mondaylunch.gatos.core.graph.Graph;
 import club.mondaylunch.gatos.core.graph.Node;
 
 public class AddElementToListNodeTest {
-    private static final List<Number> TEST_NUM_LIST = List.of(1, 2, 3);
+    private static final List<Double> TEST_NUM_LIST = List.of(1.0, 2.0, 3.0);
     private static final List<Boolean> TEST_EMPTY_LIST = new LinkedList<>();
 
     @Test
@@ -44,7 +44,7 @@ public class AddElementToListNodeTest {
     public void correctlyAddsToEmptyList() {
         var node = Node.create(BasicNodes.ADD_ELEM_TO_LIST);
         Map<String, DataBox<?>> inputs = Map.of(
-            "list", ListDataType.GENERIC_LIST.create(TEST_EMPTY_LIST),
+            "list", DataType.BOOLEAN.listOf().create(TEST_EMPTY_LIST),
             "element", DataType.ANY.create(true)
         );
 
@@ -60,32 +60,41 @@ public class AddElementToListNodeTest {
     public void correctlyAddsToExistingList() {
         var node = Node.create(BasicNodes.ADD_ELEM_TO_LIST);
         Map<String, DataBox<?>> inputs = Map.of(
-            "list", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST),
-            "element", DataType.ANY.create(4)
+            "list", DataType.NUMBER.listOf().create(TEST_NUM_LIST),
+            "element", DataType.NUMBER.create((double) 4)
         );
         Map<String, DataType<?>> inputTypes = Map.of(
             "list", DataType.NUMBER.listOf(),
             "element", DataType.NUMBER
         );
         var output = BasicNodes.ADD_ELEM_TO_LIST.compute(inputs, node.settings(), inputTypes);
-        Assertions.assertEquals(List.of(1, 2, 3, 4), output.get("output").join().value());
+        Assertions.assertEquals(List.of(1.0, 2.0, 3.0, 4.0), output.get("output").join().value());
+
+        inputs = Map.of(
+            "list", DataType.BOOLEAN.listOf().create(List.of(true, false, true)),
+            "element", DataType.BOOLEAN.create(false)
+        );
+        inputTypes = Map.of(
+            "list", DataType.BOOLEAN.listOf(),
+            "element", DataType.BOOLEAN
+        );
+        output = BasicNodes.ADD_ELEM_TO_LIST.compute(inputs, node.settings(), inputTypes);
+        Assertions.assertEquals(List.of(true, false, true, false), output.get("output").join().value());   
     }
 
     @Test
-    public void correctlyIgnoresDifferentTypes() {
+    public void correctlyIgnoresIncompatibleTypes() {
         var node = Node.create(BasicNodes.ADD_ELEM_TO_LIST);
         Map<String, DataBox<?>> inputs = Map.of(
-            "list", ListDataType.GENERIC_LIST.create(TEST_NUM_LIST),
-            "element", DataType.ANY.create("true")
+            "list", DataType.NUMBER.listOf().create(TEST_NUM_LIST),
+            "element", DataType.STRING.create("true")
         );
 
         Map<String, DataType<?>> inputTypes = Map.of(
             "list", DataType.NUMBER.listOf(),
             "element", DataType.STRING
         );
-        // Assertions.assertThrows(IllegalArgumentException.class, () -> BasicNodes.ADD_ELEM_TO_LIST.compute(inputs, node.settings(), inputTypes));
         
-        var output = BasicNodes.ADD_ELEM_TO_LIST.compute(inputs, node.settings(), inputTypes).get("output").join().value();
-        Assertions.assertEquals(List.of(1, 2, 3), output);
+        Assertions.assertThrows(NoSuchElementException.class, () -> BasicNodes.ADD_ELEM_TO_LIST.compute(inputs, node.settings(), inputTypes));
     }
 }
