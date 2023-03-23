e8a197ae7c57ccba8eb75f786f5f07b07e205931
Paul Makles <me@insrt.uk>
2023-03-23 21:08:35 +0000

    fix: remove Cors configuration on backend
    It is not needed for communication with the backend.

--- api/src/main/java/club/mondaylunch/gatos/api/ApiApplication.java
+++ api/src/main/java/club/mondaylunch/gatos/api/ApiApplication.java
@@ -3,12 +3,9 @@ package club.mondaylunch.gatos.api;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import club.mondaylunch.gatos.core.Database;
 import club.mondaylunch.gatos.core.GatosCore;
@@ -16,27 +13,14 @@ import club.mondaylunch.gatos.core.GatosCore;
 @SpringBootApplication
 @RestController
 public class ApiApplication {
-
     public static void main(String[] args) {
         Database.checkConnection();
         GatosCore.gatosInit();
         SpringApplication.run(ApiApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NotNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("*");
-            }
-        };
-    }
-
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "Us") String name) {
-        return String.format("Among %s", name);
+    public String hello() {
+        return String.format("Gatos");
     }
 }
