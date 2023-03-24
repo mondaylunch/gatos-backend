1cec56bd7f714018330f7c3c0224c0abd3cd749e
“fenn7” <afenn666@gmail.com>
2023-03-24 14:07:42 +0000

    feat: add 1 more funny

--- api/src/main/java/club/mondaylunch/gatos/api/ApiApplication.java
+++ api/src/main/java/club/mondaylunch/gatos/api/ApiApplication.java
@@ -32,6 +32,7 @@ public class ApiApplication {
     private static final String[] EASTER_EGGS = new String[] {
         "<iframe width=\"1366\" height=\"651\" src=\"https://www.youtube.com/embed/QH2-TGUlwu4\" title=\"Nyan Cat [original]\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
         "<iframe width=\"1366\" height=\"482\" src=\"https://www.youtube.com/embed/9cPxh2DikIA\" title=\"Whopper Whopper (Extended)\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
+        "<iframe width=\"956\" height=\"538\" src=\"https://www.youtube.com/embed/lW6ekG-FFiM\" title=\"Cats headbutt vine boom\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
         "<html><head></head><body><style>@keyframes wobble {0% { transform: scale(0.5); }50% { transform: scale(1.2); }100% { transform: scale(0.5); }}</style><h1 style=\"text-align:center;\">JEREON KEPPENS</h1><img src=\"https://nms.kcl.ac.uk/jeroen.keppens/images/jeroen.jpg\" style=\"margin-left:auto;margin-right:auto;animation-name: wobble;animation-play-state: running;animation-duration: 0.5s;animation-iteration-count: infinite;margin-left: auto;margin-right: auto;\"></body></html>"
     };
 }
